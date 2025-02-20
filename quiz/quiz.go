250904c419990feb8567ec694455ce6a7456c564
jokkojja <frolovdata0238@gmail.com>
2025-02-20 22:11:58 +0700

    Make game with console params using flag

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -1,99 +1,102 @@
-package main;
-import "fmt"
+package main
+
 import (
-  "encoding/csv"
-  "os"
-  "strconv"
-  "time"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
 )
 
 type Problem struct {
-  question string 
-  answer int
+	question string
+	answer   int
 }
 
 type Problems struct {
-  problems []Problem
+	problems []Problem
 }
 
-func FromFile(file *os.File) Problems{
-  var problems Problems = Problems{}
+func FromFile(file *os.File) Problems {
+	var problems Problems
 
-  reader := csv.NewReader(file)
+	reader := csv.NewReader(file)
 
-  reader.Comma = ','
-  reader.FieldsPerRecord = -1
+	reader.Comma = ','
+	reader.FieldsPerRecord = -1
 
-  records, err := reader.ReadAll();
-  
-  if err != nil {
-    panic(fmt.Errorf("Can't parse file. Err: %w", err))
-  }
-  for _, val := range records{
-    ans, _ := strconv.ParseInt(val[1], 10, 8)
-    problems.problems = append(problems.problems, Problem{
-        question: val[0],
-        answer: int(ans),
-      })
-  }
+	records, err := reader.ReadAll()
+	if err != nil {
+		panic(fmt.Errorf("Can't parse file. Err: %w", err))
+	}
 
-  return problems
+	for _, val := range records {
+		ans, _ := strconv.ParseInt(val[1], 10, 8)
+		problems.problems = append(problems.problems, Problem{
+			question: val[0],
+			answer:   int(ans),
+		})
+	}
+
+	return problems
 }
 
-func timer(secs_for_quiz int, ch chan bool) {
-  timer := time.NewTimer(time.Duration(secs_for_quiz) * time.Second)
-  <-timer.C
-  ch <- true
+func timer(secsForQuiz int, ch chan bool) {
+	timer := time.NewTimer(time.Duration(secsForQuiz) * time.Second)
+	<-timer.C
+	ch <- true
 }
 
-func game(){
-  
-  ch := make(chan bool)
-  answCh := make(chan int)
+func game(filePath string, secsForQuiz int) {
+	ch := make(chan bool)
+	answCh := make(chan int)
 
-  file, err := os.Open("quiz/problems.csv")
-  defer file.Close()
-  if err != nil{
-    panic(fmt.Errorf("Can't open file. Err: %w", err))
-  }
+	file, err := os.Open(filePath)
+	defer file.Close()
+	if err != nil {
+		panic(fmt.Errorf("Can't open file. Err: %w", err))
+	}
 
-  var problems Problems = FromFile(file);
-  var total_right_answers int = 0;
-  var total_wrong_answers int = 0;
+	var problems Problems = FromFile(file)
+	var totalRightAnswers int
+	var totalWrongAnswers int
 
-  fmt.Println("Game started. Please, answer the questions");
+	fmt.Println("Game started. Please, answer the questions")
 
-  go timer(5, ch)
-  
-  for _, problem := range problems.problems{
-    go func() {
-      var user_answer int;
-      fmt.Println(problem.question);
-      fmt.Scanln(&user_answer)
-      answCh <- user_answer;
-    }()
+	go timer(secsForQuiz, ch)
 
-    select {
-    case is_game_finished := <-ch:
-      if is_game_finished == true{
-        fmt.Println("Time is up! U lost!")
-        return
-      }
+	for _, problem := range problems.problems {
+		go func(problem Problem) {
+			var userAnswer int
+			fmt.Println(problem.question)
+			fmt.Scanln(&userAnswer)
+			answCh <- userAnswer
+		}(problem)
 
-    case user_answer := <- answCh:
-      if user_answer == problem.answer {
-        total_right_answers++;
-      } else {
-        total_wrong_answers++;
-      }
-    }
-  }
+		select {
+		case isGameFinished := <-ch:
+			if isGameFinished {
+				fmt.Printf("Time (%v seconds) is up! You lost!", secsForQuiz)
+				return
+			}
 
-    fmt.Println("Game finished. Total right answers: ", total_right_answers, " total wrong ansers: ", total_wrong_answers);
+		case userAnswer := <-answCh:
+			if userAnswer == problem.answer {
+				totalRightAnswers++
+			} else {
+				totalWrongAnswers++
+			}
+		}
+	}
+
+	fmt.Println("Game finished. Total right answers:", totalRightAnswers, "Total wrong answers:", totalWrongAnswers)
 }
 
-func main(){
-  
-  game()
+func main() {
+	filePath := flag.String("filePath", "problems.csv", "Path to file with quiz")
+	secsForQuiz := flag.Int("secsForQuiz", 30, "Secs for quizing")
+	flag.Parse()
 
+	game(*filePath, *secsForQuiz)
 }
